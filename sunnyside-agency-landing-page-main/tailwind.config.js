f15b6da1be10446a5f4faf1e783ec7c0fc59f0b3
Paul Alexander Sierra Cordova <paul.sierra1@unmsm.edu.pe>
2024-04-17 22:58:12 -0500

    Front advance 90%

---
+++ sunnyside-agency-landing-page-main/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    fontFamily:{
+      'barlow':'Barlow, sans-serif',
+      'fraunces':'Fraunces, sans-serif'
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
